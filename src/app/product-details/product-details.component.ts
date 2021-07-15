75fb4f9284d5d3b7363c41038a3955f93ecb985c
fruitfulfuture9 <fruitfulfuture9@gmail.com>
2021-07-14 18:14:09 -0700

    calculate age function and display

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute } from '@angular/router';
-import {products} from '../products';
-import {CartService} from '../cart.service';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,17 +11,20 @@ import {CartService} from '../cart.service';
 export class ProductDetailsComponent implements OnInit {
   product;
   today: number = Date.now();
-  sampleDate: Date = new Date('1948-09-09');
+  sampleDate: Date = new Date('1980-04-20');
+  age: Number;
 
   constructor(
     private route: ActivatedRoute,
     private cartService: CartService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
     });
+
+    this.age = this.calculateAge(this.sampleDate);
   }
 
   addToCart(product) {
@@ -29,4 +32,9 @@ export class ProductDetailsComponent implements OnInit {
     window.alert('your product is added to the cart!');
   }
 
+  calculateAge(dob: Date): Number {
+    let timeDiff = Math.abs(Date.now() - dob.getTime());
+    let age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+    return age;
+  }
 }
